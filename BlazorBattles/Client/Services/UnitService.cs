f6ded2fcd9d29d98336510b600359f732d495cba
Max Biocca <max.biocca@ras.rsm.de>
2022-10-25 10:22:22 +0200

    api

--- BlazorBattles/Client/Services/UnitService.cs
+++ BlazorBattles/Client/Services/UnitService.cs
@@ -1,22 +1,34 @@
 ﻿using BlazorBattles.Shared;
+using System.Net.Http.Json;
 
 namespace BlazorBattles.Client.Services
 {
     public class UnitService : IUnitService
     {
-        public IList<Unit> Units => new List<Unit>()
+        private readonly HttpClient _http;
+
+        public UnitService(HttpClient http)
         {
-            new Unit() {Id=1, Title = "Knight", Attack=10, Defense=10, BananaCost=100},
-            new Unit() {Id=2, Title = "Archer", Attack=15, Defense=5, BananaCost=150},
-            new Unit() {Id=3, Title = "Mage", Attack=20, Defense=1, BananaCost=200}
-        };
+            _http = http;
+        }
+        public IList<Unit> Units { get; set; } = new List<Unit>();
+       
         public IList<UserUnit> MyUnits { get; set; } = new List<UserUnit>();
+        IList<Unit> IUnitService.Units { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
         public void AddUnit(int unitId)
         {
             var unit = Units.First(unit => unit.Id == unitId);
             MyUnits.Add(new UserUnit { UnitId =unit.Id, HitPoints = unit.HitPoints });
 
+        }
+
+        public async Task LoadUnitAsync()
+        {
+            if (Units.Count == 0)
+            {
+                Units = await _http.GetFromJsonAsync<IList<Unit>>("api/Unit");
+            }
         }
     }
 }
